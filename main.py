3c3d97d88bb418b52744d749cc0f3dae70adff7e
oli stocks <stocksolly@gmail.com>
2024-12-02 23:04:03 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+class user:
+    
+    def __init__(self, id):
+        self.id = id
+        self.weight = 0.5
+
+    def __str__(self):
+        return f"{self.id},{self.weight}"
+    
+    def update_weight(post_id, vote_or_ignore):
+        return 
+    
+    def post_interact():
+        return
+
+
+x = user(345)
+print(str(x))
