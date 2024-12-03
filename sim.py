950ad52a671728d6646c267b2e0a34a593545fad
oli stocks <stocksolly@gmail.com>
2024-12-03 21:48:26 +0700

    :bug: code runs but returned user weight is wrong

--- sim.py
+++ sim.py
@@ -14,13 +14,12 @@ class user:
         return f"{self.id},{self.weight}"
     
     def update_weight(self, upvote_or_ignore, post_weight):
-        self.weight = modified_sigmoid((post_weight-self.weight)*upvote_or_ignore)
-        print("Updated user weight: " + str(self.weight))
+        self.weight = sigmoid((post_weight-self.weight)*upvote_or_ignore+self.weight)
     
     def post_interact(self, post_id, upvote_or_ignore, post_weight):
         self.history[post_id]=upvote_or_ignore
-        if self.weight:
-            update_weight(upvote_or_ignore, post_weight)
+        if self.weight != None:
+            self.update_weight(upvote_or_ignore, post_weight)
         else:
             print("User not yet initialised\n")
         return
@@ -34,8 +33,11 @@ class post:
         self.id = id
         self.weight = None
 
-    def initialise(self, wieght):
+    def initialise(self, weight):
         self.weight = weight
+    
+    def get():
+        return self.weight
 
     def __str__(self):
         return f"{self.id},{self.weight}"
@@ -49,6 +51,8 @@ def sigmoid(x):
     return 1/(1 + exp(-x))
 
 x = user(3)
-print(str(x))
-x.initialise()
-print(str(x))
+x.initialise(0)
+print("initial weight: " + str(x))
+x.post_interact(3, 0, 0.5)
+print("updated weight: " + str(x))
+print(x.history)
